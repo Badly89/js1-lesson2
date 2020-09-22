8c82f95e6a65d840b4834fdf5ccf4b33d40d7538
Alexey <malykhinal@mail.ru>
2020-09-22 21:30:31 +0500

    Initial commit

---
+++ dist/script2.dev.js
@@ -0,0 +1,116 @@
+"use strict";
+
+function operations() {
+  if (document.getElementById('first_number').value !== '' && document.getElementById('second_number').value !== '') {
+    var a = parseInt(document.getElementById('first_number').value);
+    var b = parseInt(document.getElementById('second_number').value);
+
+    if (a >= 0 && b >= 0) {
+      var c = a - b;
+      document.getElementById('result').innerHTML = "Разность чисел: " + c;
+    } else if (a < 0 && b < 0) {
+      c = a * b;
+      document.getElementById('result').innerHTML = "Произведение чисел: " + c;
+    } else {
+      c = a + b;
+      document.getElementById('result').innerHTML = "Сумма чисел: " + c;
+    }
+  } else {
+    alert("Введите числа!!");
+  }
+}
+
+function calcul_sum() {
+  if (document.getElementById('first_number').value !== '' && document.getElementById('second_number').value !== '') {
+    var a = parseInt(document.getElementById('first_number').value);
+    var b = parseInt(document.getElementById('second_number').value);
+    document.getElementById('result').innerHTML = "Сумма чисел: " + sum(a, b);
+  } else {
+    alert("Введите числа!!");
+  }
+}
+
+function calcul_dif() {
+  if (document.getElementById('first_number').value !== '' && document.getElementById('second_number').value !== '') {
+    var a = parseInt(document.getElementById('first_number').value);
+    var b = parseInt(document.getElementById('second_number').value);
+    document.getElementById('result').innerHTML = "Разность чисел: " + difference(a, b);
+  } else {
+    alert("Введите числа!!");
+  }
+}
+
+function calcul_mult() {
+  if (document.getElementById('first_number').value !== '' && document.getElementById('second_number').value !== '') {
+    var a = parseInt(document.getElementById('first_number').value);
+    var b = parseInt(document.getElementById('second_number').value);
+    document.getElementById('result').innerHTML = "Произведение чисел: " + multiplication(a, b);
+  } else {
+    alert("Введите числа!!");
+  }
+}
+
+function calcul_devis() {
+  if (document.getElementById('first_number').value !== '' && document.getElementById('second_number').value !== '') {
+    var a = parseInt(document.getElementById('first_number').value);
+    var b = parseInt(document.getElementById('second_number').value);
+
+    if (b !== 0) {
+      document.getElementById('result').innerHTML = "Деление чисел: " + division(a, b);
+    } else {
+      alert("Делить на ноль нельзя");
+    }
+  } else {
+    alert("Введите числа!!");
+  }
+}
+
+function sum(a, b) {
+  var c = a + b;
+  return c;
+}
+
+function difference(a, b) {
+  return c = a - b;
+}
+
+function multiplication(a, b) {
+  return c = a * b;
+}
+
+function division(a, b) {
+  return c = a / b;
+}
+
+function multiOpercall() {
+  if (document.getElementById('first_number').value !== '' && document.getElementById('second_number').value !== '') {
+    var a = parseInt(document.getElementById('first_number').value);
+    var b = parseInt(document.getElementById('second_number').value);
+    var mathoper = document.getElementById('mathoper').value;
+    multiOper(a, b, mathoper);
+  } else {
+    alert("Введите числа!!");
+  }
+}
+
+function multiOper(a, b, mathoper) {
+  switch (mathoper) {
+    case "+":
+      document.getElementById('result').innerHTML = "Сумма чисел: " + sum(a, b);
+      break;
+
+    case "-":
+      document.getElementById('result').innerHTML = "Разность чисел: " + difference(a, b);
+      break;
+
+    case "*":
+      document.getElementById('result').innerHTML = "Произведение чисел: " + multiplication(a, b);
+      break;
+
+    case "/":
+      document.getElementById('result').innerHTML = "Деление чисел: " + division(a, b);
+      break;
+  }
+
+  console.log(mathoper);
+}
---
+++ script.js
@@ -0,0 +1,73 @@
+var r = (Math.random() * 15).toFixed(0);
+switch (true) {
+    case (r <= 0):
+        r++;
+        document.write(r + "<br>");
+        // console.log(r);
+    case (r < 1):
+        r++;
+        document.write(r + "<br>");
+        // console.log(r);
+    case (r < 2):
+        r++;
+        document.write(r + "<br>");
+        // console.log(r);
+    case (r < 3):
+        r++;
+        document.write(r + "<br>");
+        // console.log(r);
+    case (r < 4):
+        r++;
+        document.write(r + "<br>");
+        // console.log(r);
+    case (r < 5):
+        r++;
+        document.write(r + "<br>");
+        // console.log(r);
+    case (r < 6):
+        r++;
+        document.write(r + "<br>");
+        // console.log(r);
+    case (r < 7):
+        r++;
+        document.write(r + "<br>");
+        // console.log(r);
+    case (r < 8):
+        r++;
+        document.write(r + "<br>");
+        // console.log(r);
+    case (r < 9):
+        r++;
+        document.write(r + "<br>");
+        // console.log(r);
+    case (r < 10):
+        r++;
+        document.write(r + "<br>");
+        // console.log(r);
+    case (r < 11):
+        r++;
+        document.write(r + "<br>");
+        // console.log(r);
+    case (r = 12):
+        r++;
+        document.write(r + "<br>");
+        // console.log(r);
+    case (r < 13):
+        r++;
+        document.write(r + "<br>");
+        // console.log(r);
+    case (r < 14):
+        r++;
+        document.write(r + "<br>");
+        // console.log(r);
+    case (r < 15):
+        r++;
+        document.write(r + "<br>");
+        // console.log(r);
+    default:
+
+        //document.write(r + "<br>");
+
+
+
+}
